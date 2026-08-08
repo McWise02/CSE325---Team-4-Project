309fa66a4cc935b48a8ff7d52e967678a88694ad
iheanyi11 <ejeagbaakuegbo@gmail.com>
2026-08-08 01:19:21 +0100

    MealEntry model and migration

--- Recipe and Meal Tracker/Data/AppDbContext.cs
+++ Recipe and Meal Tracker/Data/AppDbContext.cs
@@ -7,4 +7,20 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
     : DbContext(options)
 {
     public DbSet<Recipe> Recipes => Set<Recipe>();
+    public DbSet<MealEntry> MealEntries => Set<MealEntry>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MealEntry>()
+            .HasOne(m => m.Recipe)
+            .WithMany()
+            .HasForeignKey(m => m.RecipeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Speeds up the calendar queries (fetch all entries for a date range + meal type)
+        modelBuilder.Entity<MealEntry>()
+            .HasIndex(m => new { m.Date, m.MealType });
+    }
 }
